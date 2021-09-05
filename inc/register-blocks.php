74e1da5cf5b70b8b447f33d919a9fdc3d88514cf
kmilocollado <kmilocollado@gmail.com>
2021-09-04 23:45:23 -0400

    files services

---
+++ inc/register-blocks.php
@@ -0,0 +1,10 @@
+acf_register_block_type(array(
+    'name'              => 'services-post',
+    'title'             => __('services post),
+    'description'       => __('services'),
+    'render_template'   => 'template-parts/blocks/services-post/index.php',
+    'icon'              => 'admin-comments',
+    'keywords'          => array('noticias', 'seccion'),
+    'category' => 'widgets'
+
+));
