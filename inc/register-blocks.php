4394a7cf9dc3ac778565069f47f0a8b1819a56d7
kmilocollado <kmilocollado@gmail.com>
2021-09-04 23:58:27 -0400

    corrigiendo

--- inc/register-blocks.php
+++ inc/register-blocks.php
@@ -1,7 +1,8 @@
 <?php
+
 acf_register_block_type(array(
     'name'              => 'services-post',
-    'title'             => __('services post),
+    'title'             => __('services post'),
     'description'       => __('services'),
     'render_template'   => 'template-parts/blocks/services-post/index.php',
     'icon'              => 'admin-comments',
